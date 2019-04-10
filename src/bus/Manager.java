6c98bebe8bc8a11cb8712fdef9e009f4d17ebd91
shehab-khamis <48601737+shehab-khamis@users.noreply.github.com>
2019-04-10 07:17:51 -0700

    Add files via upload

--- src/bus/Manager.java
+++ src/bus/Manager.java
@@ -6,7 +6,11 @@
 package bus;
 
 import static bus.Bus.trip;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 
 /**
@@ -16,7 +20,8 @@ import java.util.HashMap;
 public class Manager extends Person {
       HashMap Pass = new HashMap();
       TripList object = new TripList();
-      Trip tr = new Trip();
+    //  Trip tr = new Trip();
+     // Trip x = new Trip();
       
     /**
      *
@@ -41,21 +46,78 @@ public class Manager extends Person {
         
     
 }
-    public void cancel(Trip token) throws FileNotFoundException{
+    public static boolean Write(Trip tri,String FilePath,boolean appendType) throws IOException
+    {
+        PrintWriter Writter = null;
+        try {
+            Writter = new PrintWriter(new FileWriter(new File(FilePath),appendType));
+                    
+                    Writter.printf(tri.getDestination());
+                    Writter.printf(" ");
+                    Writter.print(tri.getDays());
+                    Writter.printf(" ");
+                    Writter.printf(tri.getWay());
+                    Writter.printf(" ");
+                    Writter.printf(tri.geteKind());
+                    Writter.printf(" ");
+                    Writter.printf(tri.getStops());
+                    Writter.printf(" ");
+                    Writter.println(tri.getVeh());
+                    
+                    
+                    return true;
+        }
+        catch(IOException e){
+            System.err.println(e);
+        }
+        finally{
+            Writter.close();
+        }
+          return false;
+        
+        
+        
+    }
+    public void cancel(Trip token) throws FileNotFoundException, IOException{
        
         for (int x=0;x<trip.size();x++)
         {
             if(trip.get(x)==token)
             {
-                trip.remove(x);
+                trip.remove(x);  
                 
-            } 
-            
-        }
+            }
+          
         
+    }
+       
+                Write(trip.get(0),"E:\\shehab\\programming\\Bus Station\\Bus\\trips.txt" , false);
+                for (int i =1;i<trip.size();i++)
+                {
+                     Write(trip.get(0),"E:\\shehab\\programming\\Bus Station\\Bus\\trips.txt" , true);
+                }
+    
+    
+    
+}
+    
+    public boolean add(Trip x) throws IOException{
+        trip.add(x);
+        return Write(x,"E:\\shehab\\programming\\Bus Station\\Bus\\trips.txt",true);
         
     }
     
     
-    
+    public boolean assign(Driver dr,Trip tripp)
+    {
+        if( dr.Availability(dr.Name)==true)
+        {
+            tripp.setDriver(dr.Name);
+            return true;
+            
+        }
+        else 
+            return false;
+       
+    }
 }
